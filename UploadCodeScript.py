f1fe60b735b2747c4731f43ca9baa367514ebad5
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-09-26 22:35:21 +1000

    Add: Code upload script

---
+++ UploadCodeScript.py
@@ -0,0 +1,22 @@
+from re import M
+from time import sleep
+from MPI import get_IP
+from MPI import get_num_nodes
+import requests
+
+file = "MPImatrixmultiply"
+
+for i in range(get_num_nodes()):
+    url = 'http://' + get_IP(i) + ':8081/upload/' + file
+
+    myobj = bytearray()
+
+    f = open(file + ".py","rb")
+
+    myobj = f.read()
+
+    print(myobj)
+
+    f.close()
+
+    x = requests.post(url, myobj)
