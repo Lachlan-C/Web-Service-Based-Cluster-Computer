2bef2585f6a2d1b7636d16f7f6b42bdf5edf2328
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-10-02 20:24:38 +1100

    FIX

--- UploadCodeScript.py
+++ UploadCodeScript.py
@@ -3,8 +3,9 @@ from time import sleep
 from MPI import get_IP
 from MPI import get_num_nodes
 import requests
+import sys
 
-file = "MPImatrixmultiply.py"
+file = str(sys.argv[1])
 
 for i in range(get_num_nodes()):
     url = 'http://' + get_IP(i) + ':8081/upload/' + file
