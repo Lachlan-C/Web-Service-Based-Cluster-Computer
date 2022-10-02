00ec414480d7186f40e88afb7d9cb92172c47c26
Lachlan-C <52726897+Lachlan-C@users.noreply.github.com>
2022-10-02 20:34:47 +1100

    Update

--- RUNCODE.py
+++ RUNCODE.py
@@ -2,8 +2,9 @@ from time import sleep
 from MPI import get_IP
 from MPI import get_num_nodes
 import requests
+import sys
 
-file = "MPImatrixmultiply"
+file = str(sys.argv[1])[:len(str(sys.argv[1]))-3]
 
 for i in range(get_num_nodes()):
     url = 'http://' + get_IP(i) + ':8081/run/' + file
